5caefcb1f61e80b878cc3c6fa02d7d5dc02d3e89
angelo-dlcrz <angelo.delacruz@student.ateneo.edu>
2024-03-12 00:04:27 +0800

    Now serves content with a view

--- listings/views.py
+++ listings/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def hello(request):
+    return HttpResponse('<h1>Hello Django!</h1>')
+
+def about(request):
+    return HttpResponse('<h1>About Us</h1> <p>We love merch!</p>')
+
+def listings(request):
+    return HttpResponse('<h1>Listings</h1> <p>These are our merch!</p>')
+
+def contact(request):
+    return HttpResponse("<h1>Contact Us</h1> <p>We'd love to hear from you!</p>")
