ad2f5e550795b22eac0782c7ba554f2a5028c4a1
angelo-dlcrz <angelo.delacruz@student.ateneo.edu>
2024-03-12 00:56:55 +0800

    Now saves data to a database with a model and a migration

--- listings/views.py
+++ listings/views.py
@@ -1,14 +1,35 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from listings.models import Band, Listing
 
 def hello(request):
-    return HttpResponse('<h1>Hello Django!</h1>')
+    bands = Band.objects.all()
+    return HttpResponse(f"""
+        <h1>Hello Django!</h1>
+        <p>My favorite bands are:<p>
+        <ul>
+            <li>{ bands[0].name }</li>
+            <li>{ bands[1].name }</li>
+            <li>{ bands[2].name }</li>
+            <li>{ bands[3].name }</li>             
+        </ul>
+    """)
 
 def about(request):
     return HttpResponse('<h1>About Us</h1> <p>We love merch!</p>')
 
 def listings(request):
-    return HttpResponse('<h1>Listings</h1> <p>These are our merch!</p>')
+    listings = Listing.objects.all()
+    return HttpResponse(f"""
+        <h1>Hello Django!</h1>
+        <p>The available merch listings are:<p>
+        <ul>
+            <li>{ listings[0].title }</li>
+            <li>{ listings[1].title }</li>
+            <li>{ listings[2].title }</li>
+            <li>{ listings[3].title }</li>             
+        </ul>
+    """)
 
 def contact(request):
     return HttpResponse("<h1>Contact Us</h1> <p>We'd love to hear from you!</p>")
