5790d967204351bfea8d17c02b2772fd92437fe6
angelo-dlcrz <angelo.delacruz@student.ateneo.edu>
2024-03-13 22:57:33 +0800

    Now separates app logic from presentation using templates

--- listings/views.py
+++ listings/views.py
@@ -4,32 +4,20 @@ from listings.models import Band, Listing
 
 def hello(request):
     bands = Band.objects.all()
-    return HttpResponse(f"""
-        <h1>Hello Django!</h1>
-        <p>My favorite bands are:<p>
-        <ul>
-            <li>{ bands[0].name }</li>
-            <li>{ bands[1].name }</li>
-            <li>{ bands[2].name }</li>
-            <li>{ bands[3].name }</li>             
-        </ul>
-    """)
+    return render(request, 
+                  'listings/hello.html',
+                  {'bands': bands},)
 
 def about(request):
-    return HttpResponse('<h1>About Us</h1> <p>We love merch!</p>')
+    return render(request,
+                  'listings/about.html',)
 
 def listings(request):
     listings = Listing.objects.all()
-    return HttpResponse(f"""
-        <h1>Hello Django!</h1>
-        <p>The available merch listings are:<p>
-        <ul>
-            <li>{ listings[0].title }</li>
-            <li>{ listings[1].title }</li>
-            <li>{ listings[2].title }</li>
-            <li>{ listings[3].title }</li>             
-        </ul>
-    """)
+    return render(request,
+                  'listings/listings.html',
+                  {'listings': listings},)
 
 def contact(request):
-    return HttpResponse("<h1>Contact Us</h1> <p>We'd love to hear from you!</p>")
+    return render(request,
+                  'listings/contact.html',)
