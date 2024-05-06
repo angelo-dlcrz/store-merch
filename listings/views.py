8b753d4f088b90a33378dc43567f6083b061e2ba
angelo-dlcrz <angelo.delacruz@student.ateneo.edu>
2024-05-06 17:29:59 +0800

    Added List View and Detail View for the models

--- listings/views.py
+++ listings/views.py
@@ -2,21 +2,39 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from listings.models import Band, Listing
 
-def hello(request):
+
+def band_list(request):
     bands = Band.objects.all()
-    return render(request, 
-                  'listings/hello.html',
+    return render(request,
+                  'listings/band_list.html',
                   {'bands': bands},)
+
+
+def band_detail(request, id):
+    band = Band.objects.get(id=id)
+    return render(request,
+                  'listings/band_detail.html',
+                  {'band': band},)
+
 
 def about(request):
     return render(request,
                   'listings/about.html',)
 
-def listings(request):
+
+def listing_list(request):
     listings = Listing.objects.all()
     return render(request,
-                  'listings/listings.html',
+                  'listings/listing_list.html',
                   {'listings': listings},)
+
+
+def listing_detail(request, id):
+    listing = Listing.objects.get(id=id)
+    return render(request,
+                  'listings/listing_detail.html',
+                  {'listing': listing},)
+
 
 def contact(request):
     return render(request,
